3c1a1fbecc35651631a8f3f6c226c67fe39b278e
minison130 <minison130@gmail.com>
2018-12-31 12:57:52 +0900

    add pytorch trial

---
+++ pytorch/build/docker_build.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+this_dir=$(cd $(dirname $0); pwd)
+docker_dir=${this_dir}/../docker
+docker_path=${docker_dir}/Dockerfile.cpu
+tag=pytdj-pytorch-cpu
+
+docker build -t ${tag} ${docker_dir} -f ${docker_path}
