915da2d575b7b2cf545a1a79a728ff5303b6b8d1
minison130 <minison130@gmail.com>
2019-01-01 13:32:47 +0900

    add chainer env

---
+++ chainer/build/docker_run.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+this_dir=$(cd $(dirname $0); pwd)
+root_dir=${this_dir}/..
+src_dir=${root_dir}/src
+
+tag_name="pytdj-chainer-cpu"
+opt_mount="-v ${src_dir}:/work/src"
+opt_port="-p 8888:8888"
+
+docker run -it \
+  ${opt_mount} \
+  ${opt_port} \
+  ${tag_name}
