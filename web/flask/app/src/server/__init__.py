12d7d77fbabbd79162f528f71815de7fc03bc83a
minison130 <minison130@gmail.com>
2019-01-05 15:50:45 +0900

    add flaskapp trial

---
+++ web/flask/app/src/server/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from . import hoge
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object("config.default")
+# app.config.from_envvar("APP_CONFIG_FILE")
+
+blueprints = [hoge]
+for blueprints in blueprints:
+    app.register_blueprint(blueprint.app)
+
